e7c7c7d1d5f58f110b2640234b29e909667bf456
Michal Szpakowski <szpakk@gmail.com>
2017-05-25 15:24:42 +0200

    Complete 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo(message)
+	message
+end
+
+def shout(message)
+	message.upcase
+end
+
+def repeat(message, times=2)
+	"#{message}" + " #{message}" * (times-1)
+end
+
+def start_of_word(word, num)
+	word[0...num]
+end
+
+def first_word(sentence)
+	sentence = sentence.split(" ")
+	sentence[0]
+end
+
+def titleize(message)
+	small_words = ['and', 'of', 'from', 'to', 'a', 'the', 'over']
+	message = message.split(" ")
+	for i in 0...message.length
+		if (i == 0 || (!small_words.include? message[i]))
+			message[i].capitalize!
+		end
+	end
+	message.join(" ")
+end
