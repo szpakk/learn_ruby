47afa4d61a915df285e352fd97b3b4e1d82c128d
Michal Szpakowski <szpakk@gmail.com>
2017-05-25 17:05:53 +0200

    Complete 05_book_titles

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+
+	def title=(title)
+		small_words = ['and', 'of', 'from', 'to', 'a', 'the', 'over', 'in', 'an']
+		title = title.split(" ")
+		for i in 0...title.length
+			if (i == 0 || (!small_words.include? title[i]))
+				title[i].capitalize!
+			end
+		end
+		@title = title.join(" ")
+	end
+
+	def title
+		@title
+	end
 end
