3af9c0f2331a9d41e890c5fd0d3e4137ec0dd50b
Michal Szpakowski <szpakk@gmail.com>
2017-05-25 16:13:44 +0200

    Refactor 04_pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,12 +4,11 @@ def translate(message)
 	message_copy = message[0..-1]
 	for i in 0...message.length
 		j = 0
-		until (vowels.include? message[i][j])
+		while (!vowels.include? message[i][j]) ||
+					(message_copy[i][0] == 'u' && 
+					( message[i][0] == 'q' || message[i][0..1] == 'sq') )
 			message_copy[i] = message_copy[i][1..-1] + message_copy[i][0]
 			j += 1
-		end
-		if (message_copy[i][0] == 'u' && (message[i][0] == 'q' || message[i][0..1] == 'sq'))
-			message_copy[i] = message_copy[i][1..-1] + message_copy[i][0]			
 		end
 		message_copy[i] += "ay"
 	end
