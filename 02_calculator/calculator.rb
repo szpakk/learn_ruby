3b026de2bc30e5c51dd19791b4ac45735a64669f
Michal Szpakowski <szpakk@gmail.com>
2017-05-25 14:37:41 +0200

    Complete 02_calculator (including multiply, power, factorial)

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,32 @@
-#write your code here
+def add(a, b)
+	a + b
+end
+
+def subtract(a, b)
+	a - b
+end
+
+def sum(list)
+	result = 0
+	list.each { |num| result += num }
+	result
+end
+
+def multiply(a, b, *args)
+	result = a * b
+	if args.length > 0
+		args.each { |arg| result *= arg }
+	end
+	result
+end
+
+def power(a, b)
+	a**b
+end
+
+def factorial(num)
+	if num == 0 || num == 1
+		return 1
+	end
+	num * factorial(num-1)
+end
